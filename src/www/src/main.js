946a867ab1614037668b1da20480681dc58845c1
godan <matsushita@godan.xyz>
2017-05-07 21:31:44 +0900

    [ modify ] URL修正

--- src/www/src/main.js
+++ src/www/src/main.js
@@ -27,7 +27,7 @@ const store = new Vuex.Store({
   },
   mutations: {
     increment (state, payload) {
-      fetch('http://db.localhost/question/rand/0/0')
+      fetch('http://amix.api.ymic-it.com/question/rand/0/0')
           .then(res => res.json()).then(res => (
             state.question = res))
     },
